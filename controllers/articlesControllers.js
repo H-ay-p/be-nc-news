3e32e902ed138cc25119ee01a961330bb776868b
“Hayp” <hanna.y.peters@gmail.com>
2025-01-28 16:02:02 +0000

    do task 4, add /articles endpoint

--- controllers/articlesControllers.js
+++ controllers/articlesControllers.js
@@ -1,15 +1,28 @@
-const fetchArticleById = require("../models/articlesModels.js");
+const {
+  fetchArticleById,
+  fetchArticles,
+} = require("../models/articlesModels.js");
 
 const getArticlesById = (req, res, next) => {
   const article_id = req.params.article_id;
 
-  fetchArticleById(article_id)
-    .then((article) => {
-      res.status(200).send(article);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  if (typeof Number(article_id) === "number") {
+    fetchArticleById(article_id)
+      .then((article) => {
+        res.status(200).send(article);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  } else {
+    res.status(400).send({ message: "invalid article id" });
+  }
 };
 
-module.exports = getArticlesById;
+const getArticles = (req, res, next) => {
+  fetchArticles().then((articles) => {
+    res.status(200).send(articles);
+  });
+};
+
+module.exports = { getArticles, getArticlesById };
--- models/articlesModels.js
+++ models/articlesModels.js
@@ -12,4 +12,17 @@ const fetchArticleById = (id) => {
     });
 };
 
-module.exports = fetchArticleById;
+const fetchArticles = () => {
+  let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes,
+    articles.article_img_url, COUNT (comments.article_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY
+    articles.article_id ORDER BY articles.created_at DESC`;
+  return db.query(queryString).then((response) => {
+    if (response.rows.length === 0) {
+      return Promise.reject({ message: "no articles to be found" });
+    } else {
+      return response.rows;
+    }
+  });
+};
+
+module.exports = { fetchArticleById, fetchArticles };
