643f33154989d27aa322a2015074731b0b560844
“Hayp” <hanna.y.peters@gmail.com>
2025-01-30 16:42:31 +0000

    task 13 add comment_count to article object when searched by id

--- models/articlesModels.js
+++ models/articlesModels.js
@@ -2,12 +2,17 @@ const db = require("../db/connection.js");
 
 const fetchArticleById = (id) => {
   return db
-    .query(`SELECT * from articles WHERE article_id=$1`, [id])
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes,
+    articles.article_img_url, COUNT (comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON 
+    articles.article_id = comments.article_id GROUP BY articles.article_id HAVING articles.article_id=$1`,
+      [id]
+    )
     .then((response) => {
       if (response.rows.length === 0) {
         return Promise.reject({ message: "article not found" });
       } else {
-        return response.rows[0];
+        return response.rows;
       }
     });
 };
