df028ea81c612c166b7496d1079be22087c2a00d
“Hayp” <hanna.y.peters@gmail.com>
2025-01-31 11:31:17 +0000

    mid task 14

--- listen.js
+++ listen.js
@@ -1,7 +1,9 @@
 const app = require("./app.js");
-const PORT = 6000;
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  console.log(`listening on port ${PORT}`);
-});
+// app.listen(PORT, (err) => {
+//   if (err) console.log(err);
+//   console.log(`listening on port ${PORT}`);
+// });
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
