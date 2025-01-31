98e83b660a3597111da9342c11ebe416933234d3
“Hayp” <hanna.y.peters@gmail.com>
2025-01-31 11:40:19 +0000

    connectin and package update

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
@@ -11,10 +15,6 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
